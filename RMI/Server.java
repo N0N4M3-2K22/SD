5081008ada8061b1fb0475f8450dad0884bef50c
Vinícius Gabriel <112590929+N0N4M3-2K22@users.noreply.github.com>
2025-02-04 21:11:30 -0300

    Update Server.java

--- RMI/Server.java
+++ RMI/Server.java
@@ -1,17 +1,21 @@
 package com.exemplo;
+
 import java.rmi.Naming;
 import java.rmi.registry.LocateRegistry;
+
 public class Server {
-public static void main(String[] args) {
-try {
-// Inicia o registro RMI na porta 1099
-LocateRegistry.createRegistry(1099);
-// Instancia o serviço e o registra
-HelloImpl hello = new HelloImpl();
-Naming.rebind("HelloService", hello);
-System.out.println("Servidor RMI pronto!");
-} catch (Exception e) {
-e.printStackTrace();
-}
-}
+    public static void main(String[] args) {
+        try {
+            // Inicia o registro RMI na porta 1099
+            LocateRegistry.createRegistry(1099);
+            
+            // Instancia o serviço e o registra
+            HelloImpl hello = new HelloImpl();
+            Naming.rebind("HelloService", hello);
+            
+            System.out.println("Servidor RMI pronto!");
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
 }
