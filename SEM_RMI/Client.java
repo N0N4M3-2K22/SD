e9c4039a79468ffdd11ba7493026ddf66722ecc7
Vinícius Gabriel <112590929+N0N4M3-2K22@users.noreply.github.com>
2025-02-04 21:12:28 -0300

    Update Client.java

--- SEM_RMI/Client.java
+++ SEM_RMI/Client.java
@@ -1,21 +1,25 @@
 package com.exemplo;
+
 import java.io.*;
 import java.net.Socket;
+
 public class Client {
-public static void main(String[] args) {
-try (Socket socket = new Socket("localhost", 5000);
-BufferedReader in = new BufferedReader(new
-InputStreamReader(socket.getInputStream()));
-PrintWriter out = new PrintWriter(socket.getOutputStream(), true)) {
-// Envia a chamada do método para o servidor
-String request = "sayHello:Usuário";
-out.println(request);
-System.out.println("Requisição enviada: " + request);
-// Recebe a resposta do servidor
-String response = in.readLine();
-System.out.println("Resposta do servidor: " + response);
-} catch (IOException e) {
-e.printStackTrace();
-}
-}
+    public static void main(String[] args) {
+        try (Socket socket = new Socket("localhost", 5000);
+             BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+             PrintWriter out = new PrintWriter(socket.getOutputStream(), true)) {
+            
+            // Envia a chamada do método para o servidor
+            String request = "sayHello:Usuário";
+            out.println(request);
+            System.out.println("Requisição enviada: " + request);
+            
+            // Recebe a resposta do servidor
+            String response = in.readLine();
+            System.out.println("Resposta do servidor: " + response);
+            
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 }
